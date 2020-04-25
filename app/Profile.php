9a2c3edd90a6e7fbfd970029ad739edf5cb2854c
yamazaki-yuki-23 <natural4083@gmail.com>
2020-04-25 17:52:03 +0900

    初期写真の変更

--- app/Profile.php
+++ app/Profile.php
@@ -11,7 +11,7 @@ class Profile extends Model
 
     public function profileImage()
     {
-        $imagePath = ($this->image) ? $this->image : 'profile/Psy0tMpnjUQIbumb25Csi1XLLdhLV2QWT2R3K4Zh.jpeg';
+        $imagePath = ($this->image) ? $this->image : 'https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-19/s150x150/94692020_270398274124587_3266959104106561536_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_ohc=I6e5MC6_ywcAX9zRltZ&oh=4685f8257dd26740c7791ab75053132a&oe=5ECDC728';
 
         return '/storage/' . $imagePath;
     }
