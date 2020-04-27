b53e6de4151a8fd5b0b307330a22770d08899e13
yamazaki-yuki-23 <natural4083@gmail.com>
2020-04-27 14:54:46 +0900

    マイグレーション ファイルの修正

--- database/migrations/2020_03_25_160428_create_profiles_table.php
+++ database/migrations/2020_03_25_160428_create_profiles_table.php
@@ -19,7 +19,7 @@ class CreateProfilesTable extends Migration
             $table->string('title')->nullable();
             $table->text('description')->nullable();
             $table->string('url')->nullable();
-            $table->string('image')->nullable();
+            $table->longText('image')->nullable();
             $table->timestamps();
 
             $table->index('user_id');
